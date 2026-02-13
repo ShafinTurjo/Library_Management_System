0f2c6e48e460e35483e17bb9da60cc4fc47f97c6
Dola138 <dola.cse.20230104138@aust.edu>
2026-02-13 18:28:23 +0600

    Update App.jsx

--- library_MS/src/App.jsx
+++ library_MS/src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Register from "./Pages/register";
 import Login from "./Pages/login";
+import Book from "./Pages/Book";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/books" element={<Book />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
