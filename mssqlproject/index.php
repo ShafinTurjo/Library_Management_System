de4b74b916de3177cca1de3be2b9fba100d8606a
Tasmia Prapty <tasmiaprapty302@gami.com>
2026-02-15 18:40:56 +0600

    Update index.php

--- mssqlproject/index.php
+++ mssqlproject/index.php
@@ -1,30 +1,30 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
     exit();
 }
+
 
 $serverName = ".\SQLEXPRESS"; 
 $database = "LibraryDB"; 
 $uid = "new";
 $pass = "1234";
 
-$connectionInfo = [
+$connectionInfo = array(
     "Database" => $database,
     "Uid" => $uid,
     "PWD" => $pass,
-    "CharacterSet" => "UTF-8",
-    "ReturnDatesAsStrings" => true
-];
+    "CharacterSet" => "UTF-8"
+);
 
- 
-sqlsrv_configure("WarningsReturnAsErrors", 0);
+
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 if (!$conn) {
     echo json_encode(["status" => "error", "message" => "Connection failed"]);
