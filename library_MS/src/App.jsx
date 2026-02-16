ac4d700c6d17f605d48d02e30cd9d00b0a99aed5
Tasmia Prapty <tasmiaprapty302@gami.com>
2026-02-16 23:40:36 +0600

    update

--- library_MS/src/App.jsx
+++ library_MS/src/App.jsx
@@ -8,21 +8,18 @@ import Login from "./Pages/Login";
 import Book from "./Pages/Book";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [showSplash, setShowSplash] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2500); 
-    return () => clearTimeout(timer);
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 2500);
   }, []);
 
-  
-  if (loading) {
+  if (showSplash) {
     return <Splash />;
   }
 
-  
   return (
     <Router>
       <Routes>
@@ -30,6 +27,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/books" element={<Book />} />
+
+        <Route path="/admin-dashboard" element={<Home />} /> 
+        <Route path="/member-dashboard" element={<Book />} />
       </Routes>
     </Router>
   );
--- mssqlproject/index.php
+++ mssqlproject/index.php
@@ -1,33 +1,23 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit();
-}
 
+$serverName = ".\SQLEXPRESS";
+$connectionOptions = [
+    "Database" => "LibraryDB",
+    "Uid" => "new",
+    "PWD" => "1234",
+    "TrustServerCertificate" => true
+];
 
-$serverName = ".\SQLEXPRESS"; 
-$database = "LibraryDB"; 
-$uid = "new";
-$pass = "1234";
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 
-$connectionInfo = array(
-    "Database" => $database,
-    "Uid" => $uid,
-    "PWD" => $pass,
-    "CharacterSet" => "UTF-8"
-);
-
-
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-
-if (!$conn) {
-    echo json_encode(["status" => "error", "message" => "Connection failed"]);
-    exit();
+if ($conn === false) {
+    die(json_encode([
+        "success" => false,
+        "message" => "Database connection failed",
+        "error" => sqlsrv_errors()
+    ]));
 }
 ?>
